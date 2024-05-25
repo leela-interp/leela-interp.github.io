3457a2f77f305536a858d5d569fa5c79f8265831
Shreyas Kapur <shreyaskapur8@gmail.com>
2024-05-24 23:03:32 -0700

    Add leelaboard.

--- src/lib/pylib.js
+++ src/lib/pylib.js
@@ -3,11 +3,11 @@ import pylibString from './pylib.py?raw';
 
 async function loadPylibInternal() {
     let pyodide = await loadPyodide();
-    // await pyodide.loadPackage("https://cdn.jsdelivr.net/pyodide/v0.25.0/full/packaging-23.1-py3-none-any.whl");
-    // await pyodide.loadPackage("https://cdn.jsdelivr.net/pyodide/v0.25.0/full/micropip-0.5.0-py3-none-any.whl");
+    await pyodide.loadPackage("https://cdn.jsdelivr.net/pyodide/v0.25.0/full/packaging-23.1-py3-none-any.whl");
+    await pyodide.loadPackage("https://cdn.jsdelivr.net/pyodide/v0.25.0/full/micropip-0.5.0-py3-none-any.whl");
     await pyodide.loadPackage("https://cdn.jsdelivr.net/pyodide/v0.25.1/full/numpy-1.26.4-cp311-cp311-emscripten_3_1_46_wasm32.whl");
-    // const micropip = pyodide.pyimport("micropip");
-    // await micropip.install("numpy");
+    const micropip = pyodide.pyimport("micropip");
+    await micropip.install("chess");
     await pyodide.FS.writeFile("pylib.py", pylibString, { encoding: "utf8" });
     // Pyimport pylib.py
     return pyodide.pyimport("pylib")
